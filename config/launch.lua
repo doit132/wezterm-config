c4fa379fb866e75c308530c042ca78edd415702f
doit132 <wouitc132@qq.com>
2023-12-21 21:21:09 +0800

    修改 wezterm 默认启动 pwsh

--- config/launch.lua
+++ config/launch.lua
@@ -3,7 +3,7 @@ local platform = require('utils.platform')()
 local options = {default_prog = {}, launch_menu = {}}
 
 if platform.is_win then
-    options.default_prog = {'nu'}
+    options.default_prog = {'pwsh'}
     options.launch_menu = {
         {label = 'PowerShell Core', args = {'pwsh'}},
         {label = 'PowerShell Desktop', args = {'powershell'}},
